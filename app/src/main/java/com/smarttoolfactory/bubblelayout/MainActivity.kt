e07b642b4ddcc4103fbf82fbca504373c10ed53b
Smart Tool Factory <smarttoolfactory@icloud.com>
2021-12-22 16:59:59 +0300

    Update gradle

--- app/src/main/java/com/smarttoolfactory/bubblelayout/MainActivity.kt
+++ app/src/main/java/com/smarttoolfactory/bubblelayout/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
